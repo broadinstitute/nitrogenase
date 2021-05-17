e61eedb4fcc64e3f674f69f02366714cc8ad71ac
curoli <curoli@gmail.com>
2021-05-16 21:21:40 -0400

    Null model WDL, first draft.

--- metastaar/r/STAAR_null_model.R
+++ metastaar/r/STAAR_null_model.R
@@ -6,11 +6,15 @@ library(optparse)
 option_list <- list(
   make_option(dest = "phenotype_file", c("--phenotype-file"), type = "character", default = NULL,
               help = "The phenotype file.", metavar = "file"),
+  make_option(dest = "sample_id", c("--sample-id"), type = "character", default = NULL,
+              help = "Sample id in the phenotype file.", metavar = "sample_id"),
   make_option(dest = "phenotype", c("--phenotype"), type = "character", default = NULL, help = "The phenotype.",
               metavar = "phenotype"),
+  make_option(dest = "groups", c("--groups"), type = "character", default = NULL, help = "Groups used (optional).",
+              metavar = "phenotype"),
   make_option(dest = "grm", c("--grm"), type = "character", default = NULL, help = "The GRM file.", metavar = "grm"),
-  make_option(dest = "formula", c("--formula"), type = "character", default = NULL, help = "The model formula",
-              metavar = "formula"),
+  make_option(dest = "covariates", c("--covariates"), type = "character", default = NULL,
+              help = "Covariates as a comma-separated list", metavar = "covariates"),
   make_option(dest = "output", c("--output"), type = "character", default = NULL, help = "The output file",
               metavar = "output")
 )
@@ -26,9 +30,10 @@ assert_opt <- function(id, label) {
 }
 
 assert_opt("phenotype_file", "phenotype file")
+assert_opt("sample_id", "sample id")
 assert_opt("phenotype", "phenotype")
 assert_opt("grm", "GRM file")
-assert_opt("formula", "formula")
+assert_opt("covariates", "covariates")
 assert_opt("output", "output file")
 
 ## Phenotype
@@ -37,16 +42,18 @@ phenotype <- read.csv(opt$phenotype_file, header = TRUE, , stringsAsFactors = FA
 ##### LDL
 phenotype <- phenotype[!is.na(phenotype[[opt$phenotype]]),]
 
-## test
-lm.test <- lm(as.formula(opt$formula), data = phenotype)
-summary(lm.test)
-
 ## load GRM
 load(opt$grm)
 
-### fit null model
-obj.SMMAT.LDL <-
-  fit_null_glmmkin(as.formula(opt$formula), data = phenotype, kins = skm, kins_cutoff = 0.022, id = "sample.id",
-                   groups = "group", use_sparse = TRUE, family = gaussian(link = "identity"), verbose = T)
+covariates_string <- opt$covariates
 
-save(obj.SMMAT.LDL, file = opt$output)
+str_replace_all(covariates_string, ",", "+")
+
+formula <- as.formula(paste(opt$phenotype, "~", covariates_string))
+
+### fit null model
+null_model <-
+  fit_null_glmmkin(formula, data = phenotype, kins = skm, kins_cutoff = 0.022, id = opt$sample_id, groups = opt$groups,
+                   use_sparse = TRUE, family = gaussian(link = "identity"), verbose = T)
+
+save(null_model, file = opt$output)
---
+++ metastaar/wdl/null_model.wdl
@@ -0,0 +1,49 @@
+version 1.0
+
+workflow null_model {
+    input {
+        File phenotype_file
+        String sample_id_field
+        String phenotype
+        String? groups
+        File? kinship_matrix_file
+        Array[String] covariates
+        String out_file_name
+    }
+    call calculate_null_model {
+        input:
+            phenotype_file = phenotype_file,
+            sample_id_field = sample_id_field,
+            phenotype = phenotype,
+            groups = groups,
+            kinship_matrix_file = kinship_matrix_file,
+            covariates = covariates,
+            out_file_name = out_file_name
+    }
+}
+
+task calculate_null_model {
+    input {
+        File phenotype_file
+        String sample_id_field
+        String phenotype
+        String? groups
+        File? kinship_matrix_file
+        Array[String] covariates
+        String out_file_name
+    }
+    runtime {
+        docker: "gcr.io/nitrogenase-docker/nitrogenase-metastaar:1.2.0"
+    }
+    command <<<
+        set -e
+        echo "Now calculating covariances"
+        Rscript --verbose /r/STAAR_null_model.R --phenotype-file ~{phenotype_file} --sample-id ~{sample_id_field}
+            --phenotype ~{phenotype} ~{"--groups" + groups} ~{"--grm" + kinship_matrix_file}
+            --covariates ~{sep="," covariates} --output ~{out_file_name}
+        echo "Done calculating covariances"
+    >>>
+    output {
+        File out_file = out_file_name
+    }
+}
