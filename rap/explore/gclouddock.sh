7e420e70444d93523bb20ed2880a69182d235684
curoli <curoli@gmail.com>
2022-08-17 17:10:13 -0400

    Docker and WDL to explore data on UK Biobank RAP.

---
+++ rap/explore/gclouddock.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+project="nitrogenase-docker"
+name="nitrogenase-rap-explore"
+tag="0.1.0"
+image="${name}:${tag}"
+echo "Using Google project ${project}, Docker project ${name}, image tag ${image}"
+echo "Cloud-building Docker image:"
+full="gcr.io/${project}/${image}"
+gcloud builds submit --timeout=60m --tag $full
+arg1=$1
+if [ -n "$arg1" ];then
+  docker run -it $full "$arg1"
+fi
+echo "Done with $full $arg1"
