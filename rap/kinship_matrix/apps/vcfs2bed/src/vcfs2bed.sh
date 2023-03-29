8706b8beb082b6d0d6b78d6b3b8475c1f1197d79
curoli <curoli@gmail.com>
2023-03-29 17:40:56 -0400

    vcfs2bed: upgrading plink again.

--- rap/kinship_matrix/apps/vcfs2bed/src/vcfs2bed.sh
+++ rap/kinship_matrix/apps/vcfs2bed/src/vcfs2bed.sh
@@ -59,8 +59,8 @@ main() {
 
     mkdir plink
     cd plink
-    wget https://s3.amazonaws.com/plink2-assets/plink2_linux_x86_64_20230328.zip
-    unzip plink2_linux_x86_64_20230328.zip
+    wget https://s3.amazonaws.com/plink2-assets/plink2_linux_x86_64_20230329a.zip
+    unzip plink2_linux_x86_64_20230329a.zip
     ls -ralth
     mv plink2 /usr/local/bin/
     cd ..
