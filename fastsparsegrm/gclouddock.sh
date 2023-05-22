f15b688f260199530fa4953824730bb174701806
curoli <curoli@gmail.com>
2023-05-22 14:38:17 -0400

    Adding thin app.

--- fastsparsegrm/gclouddock.sh
+++ fastsparsegrm/gclouddock.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 project="nitrogenase-docker"
-name="nitrogenase-metastaar"
-tag="1.6.0"
+name="nitrogenase-fastsparsegrm"
+tag="0.1.0"
 full="${name}:${tag}"
 echo "Using Google project ${project}, Docker project ${name}, full tag ${full}"
 echo "Cloud-building Docker image:"
 gcloud builds submit --timeout=60m --tag gcr.io/${project}/${full}
-#echo "Building"
-#sudo docker build . -t gcr.io/${project}/${full}
-#echo "Submitting"
-#docker push gcr.io/${project}/${full}
 echo "Done"
