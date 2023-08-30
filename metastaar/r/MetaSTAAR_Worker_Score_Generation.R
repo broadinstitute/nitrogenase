655345dc54367b240ff895bdabfea584bdeb1f14
curoli <curoli@gmail.com>
2023-08-30 16:30:18 -0400

    Correctly handling the situation when there are no selected variants in a segment, part 3.

--- metastaar/r/MetaSTAAR_Worker_Score_Generation.R
+++ metastaar/r/MetaSTAAR_Worker_Score_Generation.R
@@ -97,8 +97,8 @@ write_sumstat_parquet <- function(df, path, metadata=NULL) {
 
 	ncovar = dim(df)[2] - 10
 	logDebug("ncovar", ncovar)
-	if(is.null(ncovar) | ncovar <= 0) {
-		print(paste("ncovar should be at least 1 but is ", valueDebug(ncovar)))
+	if(length(ncovar) < 1) {
+		print(paste("ncovar should not be empty but is ", valueDebug(ncovar)))
 		print("Probably because there are no selected variants in this segment.")
 		quit(save = "no", status = 20, runLast = FALSE)
 	}
